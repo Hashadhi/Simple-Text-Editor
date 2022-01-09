93b539c413bdae53fe29a15bfbbdfbb01748c81a
Hashadhi <hashadhijayasinghe@gmai.com>
2022-01-09 23:08:17 +0530

    Added features to the editor

---
+++ src/controller/TextEditorFormController.java
@@ -0,0 +1,121 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.input.Clipboard;
+import javafx.scene.input.ClipboardContent;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.DirectoryChooser;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+import javax.swing.*;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.ByteBuffer;
+import java.nio.channels.SeekableByteChannel;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+
+public class TextEditorFormController {
+    public BorderPane brdrpnArea;
+    public TextArea txtAreaEditor;
+    public TextField txtSource;
+    public Clipboard clipboard;
+
+    public void newDocOnAction(ActionEvent actionEvent) throws IOException {
+        Stage stage = (Stage) brdrpnArea.getScene().getWindow();
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/textEditorForm.fxml"))));
+        stage.show();
+
+    }
+
+    public void openOnAction(ActionEvent actionEvent) throws IOException {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Open a text file");
+        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Text Files", "*.txt"));
+
+        File file = fileChooser.showOpenDialog(null);
+
+        String location = file.getAbsolutePath();
+
+        Path path = Paths.get(file.getAbsolutePath());
+
+        byte[] bytes = Files.readAllBytes(path);
+        txtAreaEditor.setText(new String(bytes));
+    }
+
+    public void saveOnAction(ActionEvent actionEvent) throws IOException {
+        String content = txtAreaEditor.getText();
+        byte[] fileBytes = content.getBytes();
+
+        DirectoryChooser directoryChooser = new DirectoryChooser();
+
+        directoryChooser.setTitle("Open a directory");
+        File file = directoryChooser.showDialog(null);
+        String destinationPath=file.getAbsolutePath();
+
+        String fileName = JOptionPane.showInputDialog("File name: ");
+        Path path = Paths.get(destinationPath + "/" + fileName + ".txt");
+
+        SeekableByteChannel byteChannel = Files.newByteChannel(path, StandardOpenOption.WRITE, StandardOpenOption.CREATE);
+        ByteBuffer buffer = ByteBuffer.wrap(fileBytes);
+        byteChannel.write(buffer);
+        byteChannel.close();
+        Files.write(path, fileBytes);
+
+        new Alert(Alert.AlertType.INFORMATION, "Saved!").show();
+    }
+
+    public void exitOnAction(ActionEvent actionEvent) {
+        System.exit(0);
+    }
+
+    public void cutOnAction(ActionEvent actionEvent) {
+        setSelectedText();
+        String[] texts = txtAreaEditor.getText().split(txtAreaEditor.getSelectedText());
+        txtAreaEditor.clear();
+        for (String text:texts
+        ) {
+            txtAreaEditor.setText(txtAreaEditor.getText()+text);
+        }
+    }
+
+    public void copyOnAction(ActionEvent actionEvent) {
+        setSelectedText();
+    }
+
+    public void pasteOnAction(ActionEvent actionEvent) {
+        int caretPosition = txtAreaEditor.getCaretPosition();
+        txtAreaEditor.insertText(caretPosition, clipboard.getString());
+    }
+
+    public void selectAllOnAction(ActionEvent actionEvent) {
+        txtAreaEditor.selectAll();
+    }
+
+    public void aboutUsOnAction(ActionEvent actionEvent) throws IOException {
+        Parent load = FXMLLoader.load(getClass().getResource("/view/aboutUsForm.fxml"));
+        Scene scene = new Scene(load);
+        Stage stage = new Stage();
+        stage.setScene(scene);
+        stage.setResizable(true);
+        stage.show();
+    }
+    private void setSelectedText(){
+        clipboard = Clipboard.getSystemClipboard();
+        ClipboardContent content = new ClipboardContent();
+        content.putString(txtAreaEditor.getSelectedText());
+        clipboard.setContent(content);
+    }
+}
